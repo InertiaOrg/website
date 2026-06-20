30908a7fcae92cff8eeed1ee60990c9d330a7e47
Chamogelastos <stavros.ssta@gmail.com>
2026-06-20 18:50:17 +0300

    Enough web design for today

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  site: 'https://inertiaorg.github.io/website/',
+  base: '/website',
+  integrations: [
+      svelte(),
+      icon(),
+      mdx(),
+  ],
+});
